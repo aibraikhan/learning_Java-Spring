f62788a066da985d64b1d5804d71dab0262d636a
Alinur <125266339+aibraikhan@users.noreply.github.com>
2024-08-19 17:31:39 +0600

    Hello World!!!

---
+++ firstLesson.java
@@ -0,0 +1,10 @@
+
+
+
+public class firstLesson {
+
+    public static void main(String[] args) {
+        // 1. Declare and initialize a variable
+        System.out.println("Hello World");
+    }
+}
